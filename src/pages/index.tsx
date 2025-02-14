99aafc1832ad096394086978403be18c37962c89
Neomanbeta <33118244+Neomanbeta@users.noreply.github.com>
2025-02-14 21:32:03 +0800

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -31,16 +31,15 @@ class HomeComponent extends React.Component<WithTranslation> {
                     (
                         <div>
                             <div className='text-xl md:text-5xl mb-2 hidden sm:block'>
-                                A<Typist startDelay={1000}  typingDelay={110} loop={true}  cursor={<span className='cursor'>{this.state.cursor}</span>}   >nonymous Chat Room <Typist.Delay ms={1500} /><Typist.Backspace count={18} /></Typist>
-
+                                N<Typist startDelay={1000}  typingDelay={110} loop={true}  cursor={<span className='cursor'>{this.state.cursor}</span>}   >eoMan's Voice Server <Typist.Delay ms={1500} /><Typist.Backspace count={20} /></Typist>
                             </div>
                             <div className='text-xl md:text-5xl mb-2 block sm:hidden'>
-                                Anonymous Chat Room
+                                NeoMan's Voice Server
                             </div>
                         </div>
                     ) : (
                         <div className='text-xl md:text-5xl mb-2 block'>
-                            欢迎来到匿名聊天室
+                            NeoMan的语音服务器
                         </div>
                     )
                 }
